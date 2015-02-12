e081d1d472617fff5e3c5f882be6f62998caa599
Greg Back <gback@mitre.org>
2015-02-12 16:22:36 -0600

    Better handling of __str__ and __unicode__.

--- cybox/compat.py
+++ cybox/compat.py
@@ -23,3 +23,24 @@ elif is_py3:
     bytes = bytes
     long = int
     str = str
+
+
+class UnicodeMixin(object):
+    """Make String functions work on Python 2 and 3.
+
+    Classes using this mixin must define a `__unicode__` function that returns
+    the Unicode representation of the object (as a `str` on Python 3 and a
+    `unicode` on Python 2).
+
+    Python 2 will use `__unicode__()` directly for `unicode()` calls, and
+    encode the output in UTF-8 when the `str()` function is called.
+
+    Python 3 will use `__unicode__()` indirectly when `str()` is called.
+
+    This code was adapted from:
+        http://lucumr.pocoo.org/2011/1/22/forwards-compatible-python/
+    """
+    if is_py3:
+        __str__ = lambda x: x.__unicode__()
+    else:
+        __str__ = lambda x: x.__unicode__().encode('utf-8')
